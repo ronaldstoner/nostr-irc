bdfb4c057c335bb9ac3fafa434e3f54168034024
Ron Stoner <ron@team.casa>
2023-03-11 22:44:12 -0700

    Clean up of commands file and Join command structure

--- commands.py
+++ commands.py
@@ -9,13 +9,13 @@ class CommandHandler:
 
     def __init__(self):
         self.commands = {
-            '/quit': self.handle_quit,
-            '/who': self.handle_who,
-            '/part': self.handle_part,
-            '/slap': self.handle_slap,
             '/clear': self.handle_clear,
+            '/join': self.handle_join,            
+            '/part': self.handle_part,
+            '/quit': self.handle_quit,
+            '/slap': self.handle_slap,
+            '/who': self.handle_who,
         }
-
 
     def handle_command(self, command_string, messages):
         parts = command_string.split(' ')
@@ -27,13 +27,32 @@ class CommandHandler:
         if command in self.commands:
             self.commands[command](arg, messages)
 
-
     def stop_tasks(self, tasks):
         # Cancel all tasks
         for task in tasks:
             task.cancel()
 
-    # Quit command - not working gracefully atm TODO: fix this
+    # Clear command
+    def handle_clear(self, arg, messages):
+        messages.clear()
+        messages.refresh()
+        pass
+
+    # Join command
+    def handle_join(self, arg, messages):
+        # Implement part logic here
+        messages.addstr("\n *** JOIN COMMAND ENTERED\n")
+        messages.refresh()
+        pass
+
+    # Part command 
+    def handle_part(self, arg, messages):
+        # Implement part logic here
+        messages.addstr("\n *** PART COMMAND ENTERED\n")
+        messages.refresh()
+        pass
+
+    # Quit command
     def handle_quit(self, arg, messages):
         # Get all running tasks
         tasks = asyncio.all_tasks()
@@ -59,28 +78,6 @@ class CommandHandler:
         # Print goodbye message
         print("Thank you for using nostr-irc.")
 
-
-    # Who command
-    def handle_who(self, arg, messages):
-        if not arg:
-            # Top 20 recent users plus user count
-            top_names = sorted(nip_05_identifiers.items(), key=lambda x: x[1], reverse=True)[:20]
-            remaining_names_tally = len(nip_05_identifiers) - len(top_names)
-            result_string = ', '.join([f'{name}' for pubkey, name in top_names]) + f' and {remaining_names_tally} other users'
-            messages.addstr(f"\n *** Recent Users: {result_string}\n\n", curses.color_pair(1) | curses.A_DIM)
-        else:
-            messages.addstr(f"\n *** WHO COMMAND ENTERED FOR USER: {arg}\n")
-            messages.refresh()
-
-
-    # Part command for NIP-4x
-    def handle_part(self, arg, messages):
-        # Implement part logic here
-        messages.addstr("\n *** PART COMMAND ENTERED\n")
-        messages.refresh()
-        pass
-
-
     # Slap command
     def handle_slap(self, arg, messages):
         # Implement part logic here
@@ -88,8 +85,15 @@ class CommandHandler:
         messages.refresh()
         pass
 
-    # Clear command
-    def handle_clear(self, arg, messages):
-        messages.clear()
-        messages.refresh()
-        pass
+    # Who command
+    def handle_who(self, arg, messages):
+        if not arg:
+            # Top 20 recent users plus user count
+            # TODO: Add timestamps and actually make this recent
+            top_names = sorted(nip_05_identifiers.items(), key=lambda x: x[1])[:20]
+            remaining_names_tally = len(nip_05_identifiers) - len(top_names)
+            result_string = ', '.join([f'{name}' for pubkey, name in top_names]) + f' and {remaining_names_tally} other users'
+            messages.addstr(f"\n *** Recent Users: {result_string}\n\n", curses.color_pair(1) | curses.A_DIM)
+        else:
+            messages.addstr(f"\n *** WHO COMMAND ENTERED FOR USER: {arg}\n")
+            messages.refresh()
